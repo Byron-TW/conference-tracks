1f5fbdddfd6d473a70c6251c390193053dd2dcb8
Sebastian Thiel <byronimo@gmail.com>
2018-05-27 18:43:21 +0200

    a step closer to an actual solution

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 extern crate failure;
 
 use failure::{Error, ResultExt};
-use std::{io::{self, BufReader, BufWriter, Write}, str::FromStr, time::Duration};
-use std::iter::once;
+use std::{cmp, io::{self, BufReader, BufWriter, Write}, iter::once, str::FromStr, time::Duration};
 
 const HOURS: u64 = 60 * 60;
+const LIGHTNING: u64 = 5 * 60;
 const MORNING_SESSION_DURATION: Duration = Duration::from_secs(3 * HOURS);
 const MORNING_SESSION_START: Duration = Duration::from_secs(9 * HOURS);
 const EVENING_SESSION_MAX_DURATION: Duration = Duration::from_secs(4 * HOURS);
@@ -57,7 +57,7 @@ pub fn answers(input: impl io::Read, output: impl io::Write) -> Result<(), Error
             bail!("Could not schedule the remaining {} talks", talks.len())
         }
         if talks.is_empty() {
-            return Ok(())
+            return Ok(());
         } else {
             write!(output, "\n\n")?;
         }
@@ -84,36 +84,73 @@ fn format<'a>(
         let hour_of_day = current_time.as_secs() / HOURS;
         let minutes_of_hour = (current_time.as_secs() % HOURS) / 60;
         let time_of_day = if hour_of_day >= 12 { "PM" } else { "AM" };
+        let duration = if talk.duration.as_secs() == LIGHTNING {
+            "lightning".to_string()
+        } else {
+            format!("{}min", talk.duration.as_secs() / 60)
+        };
         writeln!(
             output,
-            "{} {:02}:{:02}{}",
-            talk.name, hour_of_day, minutes_of_hour, time_of_day
+            "{} {:02}:{:02}{} {}",
+            talk.name, hour_of_day, minutes_of_hour, time_of_day, duration
         )?;
         current_time += talk.duration;
     }
     Ok(current_time)
 }
 
-fn schedule(
-    _session_duration: &Duration,
-    talks: &mut Vec<Talk>,
-    session: &mut Vec<Talk>,
-) -> Result<(), Error> {
-    let mut c = 5;
-
-    while let Some(talk) = talks.pop() {
-        session.push(talk);
-        c -= 1;
-        if c == 0 {
-            return Ok(());
+/// from https://github.com/acmeism/RosettaCodeData/blob/master/Task/Knapsack-problem-0-1/Rust/knapsack-problem-0-1.rust
+/// and adjusted accordingly
+fn knapsack01_dyn(items: &[Talk], max_weight: usize, result: &mut Vec<Talk>) {
+    let mut best_value = vec![vec![0; max_weight + 1]; items.len() + 1];
+    for (i, it) in items.iter().enumerate() {
+        for w in 1..max_weight + 1 {
+            best_value[i + 1][w] = if it.weight() > w {
+                best_value[i][w]
+            } else {
+                cmp::max(
+                    best_value[i][w],
+                    best_value[i][w - it.weight()] + it.value(),
+                )
+            }
         }
+    }
+
+    let mut left_weight = max_weight;
+
+    for (i, it) in items.iter().enumerate().rev() {
+        if best_value[i + 1][left_weight] != best_value[i][left_weight] {
+            result.push(it.clone());
+            left_weight -= it.weight();
+        }
+    }
+}
+
+fn schedule(
+    session_duration: &Duration,
+    talks: &mut Vec<Talk>,
+    mut session: &mut Vec<Talk>,
+) -> Result<(), Error> {
+    knapsack01_dyn(&talks, session_duration.as_secs() as usize, &mut session);
+    for session_talk in session {
+        talks.retain(|t| t.name != session_talk.name)
     }
     Ok(())
 }
 
+#[derive(Clone)]
 struct Talk {
     name: String,
     duration: Duration,
+}
+
+impl Talk {
+    fn weight(&self) -> usize {
+        self.duration.as_secs() as usize
+    }
+    fn value(&self) -> usize {
+        self.weight()
+    }
 }
 
 impl FromStr for Talk {
@@ -133,7 +170,7 @@ impl FromStr for Talk {
         } else if s.ends_with("lightning") {
             Talk {
                 name: s[..s.len() - " lightning".len()].to_owned(),
-                duration: Duration::from_secs(5 * 60),
+                duration: Duration::from_secs(LIGHTNING),
             }
         } else {
             bail!("Could not parse talk from '{}'", s)
